32981dac1380b861421bee05cc38e43a8f65f1bb
twossh <38133710+twossh@users.noreply.github.com>
2019-07-31 17:46:53 -0300

    Update attscript.sh

--- Sistema/attscript.sh
+++ Sistema/attscript.sh
@@ -30,11 +30,11 @@ echo " "
 fun_atts () {
   [[ -e /home/versao ]] && rm /home/versao
   [[ -e /tmp/att ]] && rm /tmp/att
-	wget -c -P /home http://ssh-plus.tk/script/versao
+	wget -c -P /home https://raw.githubusercontent.com/twossh/SSHPLUS/master/Sistema/versao
 	[[ -f "/home/versao" ]] && mv /home/versao /tmp/att
 	[[ ! -e /bin/versao ]] && rm -rf /bin/menu
 } > /dev/null 2>&1
-echo -e "                              \033[1;31mBy Crazy\033[1;36m"
+echo " "
 echo -e "   SSHPlus" | figlet
 echo " "
 echo -e "   \033[1;32mVERIFICANDO ATUALIZACOES DISPONIVEIS\033[0m\n"
@@ -50,12 +50,12 @@ vrs2=$(sed -n '1 p' /tmp/att | sed -e 's/[^0-9]//ig')
 [[ "$vrs1" == "$vrs2" ]] && {
   echo -e " \033[1;36m     O SCRIPT JA ESTA ATUALIZADO!\033[1;32m\n"
   rm /tmp/att > /dev/null 2>&1
-  echo -e " \033[1;33m MAIS INFORMACOES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@crazy_vpn\n"
+  echo -e " \033[1;33m MAIS INFORMACOES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@twossh\n"
   echo -ne " \033[1;31m ENTER \033[1;33mpara retornar ao \033[1;32mMENU!\033[0m"; read
   menu
 } || {
   echo -e "  \033[1;36mEXISTE UMA NOVA ATUALIZACAO DISPONIVEL!\033[1;33m\n"
-  echo -e "  \033[1;33mMAIS INFORMACOES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@crazy_vpn\n"
+  echo -e "  \033[1;33mMAIS INFORMACOES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@twossh\n"
   echo -e "  \033[1;32mDETALHES DA ATUALIZACAO:\033[0m\n"
   while read linha; do
     echo -e "  \033[1;37m- \033[1;33m$linha"
@@ -65,7 +65,7 @@ vrs2=$(sed -n '1 p' /tmp/att | sed -e 's/[^0-9]//ig')
   if [[ "$res" = s || "$res" = S ]];then
     echo -e "\n\033[1;32m  INICIANDO ATUALIZACAO..."
     sleep 3
-    wget ssh-plus.tk/script/Plus > /dev/null 2>&1
+    wget https://raw.githubusercontent.com/twossh/SSHPLUS/master/Plus > /dev/null 2>&1
     chmod +x Plus
     ./Plus
     clear
